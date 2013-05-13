fe961b6a7569d342cd2f9f9e870f2b7b3178e8af
¨Hongliang <h.wang@linkorb.com>
2013-05-13 17:36:36 +0200

    stomp support

--- src/Notifier/Handler/StompHandler.php
+++ src/Notifier/Handler/StompHandler.php
@@ -8,11 +8,15 @@
 
 namespace Notifier\Handler;
 
+use Notifier\Notifier;
 use Notifier\Message\MessageInterface;
 use Notifier\Recipient\RecipientInterface;
+use FuseSource\Stomp\Stomp;
+use FuseSource\Stomp\Frame;
 
 class StompHandler extends AbstractHandler
 {
+    protected $deliveryType = 'stomp';
     protected $stompserver;
     protected $stompport;
     protected $stompclientid;
@@ -23,16 +27,24 @@ class StompHandler extends AbstractHandler
 	 * @param String $server The STOMP server.
 	 * @param Int $port The port number of the STOMP server.
 	 * @param String $clientid The client id.
-	 * @param String $destination The desitnation queue.
+     * @param String $destination The desitnation queue.
+     * @param string|array $types types
+     * @param boolean $bubble Bubble or not.
 	 */
-	public function __construct($server = 'localhost', $port = 61613, $clientid = 'mystomp', $destination = 'undefined')
+	public function __construct($server = 'localhost', $port = 61613, $clientid = 'mystomp', $destination = 'undefined', $types = Notifier::TYPE_ALL, $bubble = true)
     {
         $this->stompserver      = $server;
         $this->stompport        = $port;
         $this->stompclientid    = $clientid;
         $this->stompdestination = $destination;
+        parent::__construct($types, $bubble);
 	}
-    
+
+    protected function send(MessageInterface $message, RecipientInterface $recipient)
+    {
+       return $this->sendStomp($message, $recipient);
+    }
+
     private function connectSTOMP()
     {
         try {
@@ -45,12 +57,11 @@ class StompHandler extends AbstractHandler
         return $stomp;
 	}
 
-    protected function send(MessageInterface $message, RecipientInterface $recipient)
+    private function sendStomp(MessageInterface $message, RecipientInterface $recipient)
     {
         $stomp	= $this->connectSTOMP();
         $frame	= new Frame('SEND', $this->stompheaders, $message->getContent());
         $res	= $stomp->send('/queue/' . $this->stompdestination, $frame, array(), true);
-        //echo "RECEIPT: " . $frame->headers['receipt'];
         return $res;
     }
 
