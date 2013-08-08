1b74abebee77c5e1b53986a844b1345b1ba6f2f3
Dries De Peuter <dries@nousefreak.be>
2013-08-08 19:49:20 +0200

    Add some common exceptions

---
+++ src/Notifier/Exception/NotifierException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of the Notifier package.
+ *
+ * (c) Dries De Peuter <dries@nousefreak.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace Notifier\Exception;
+
+/**
+ * @author Dries De Peuter <dries@nousefreak.be>
+ */
+class NotifierException extends \Exception
+{
+}
